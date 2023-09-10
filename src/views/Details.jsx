d85a3082b7ef3a59e12e585a54ce980bc8db40f0
Gabriela Gomez <ggaabrielagomez@gmail.com>
2023-09-09 22:04:54 -0600

    Creación de vistas Categs, details y shop

---
+++ src/views/Details.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Details = () => {
+  return (
+    <View style={styles.container}>
+      <Text> Details funciona </Text>
+    </View>
+  );
+};
+
+export default Details;
---
+++ src/views/Shop.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Shop = () => {
+  return (
+    <View style={styles.container}>
+      <Text> Shop funciona </Text>
+    </View>
+  );
+};
+
+export default Shop;
