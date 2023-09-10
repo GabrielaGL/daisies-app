c6b68aa21b6a8efe8549d349e0d9add6a8066d81
Gabriela Gomez <ggaabrielagomez@gmail.com>
2023-09-09 22:51:55 -0600

    Rutas de Registro y nav bar agregadas

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,17 +2,22 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Login from "../views/Login";
+import SignUp from "../views/SignUp";
+import BottomNavBar from "../components/BottomNavBar";
+
 
 const Stack = createNativeStackNavigator();
 
 function Router() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name='Login' component={Login} />
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Registro" component={SignUp} />
+        <Stack.Screen name="BottomNavBar" component={BottomNavBar} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
 
 export default Router;
