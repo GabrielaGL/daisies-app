e225c65a1a928b87f92f95add691f36a150e7abb
Gabriela Gomez <ggaabrielagomez@gmail.com>
2023-09-11 00:10:56 -0600

    Consultas a api de categorías

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -4,7 +4,13 @@ const ApiService = () => {
   const api_url = "https://fakestoreapi.com";
 
   const [products, setProducts] = useState([]);
+  const [jewelery, setJewelery] = useState([]);
+  const [electronic, setElectronic] = useState([]);
+  const [mensClothing, setMensClothing] = useState([]);
+  const [womensClothing, setWomensClothing] = useState([]);
 
+
+  
   const getProducts = () => {
     fetch(api_url + "/products?limit=20")
       .then((res) => res.json())
@@ -12,12 +18,48 @@ const ApiService = () => {
       .catch((error) => console.error(error));
   };
 
+  const getJeweleryCategory = () => {
+    fetch(api_url + "/products/category/jewelery")
+    .then((res) => res.json())
+      .then((json) => setJewelery(json))
+      .catch((error) => console.error(error));
+  };
+
+  const getElectronicCategory = () => {
+    fetch(api_url + "/products/category/electronics")
+    .then((res) => res.json())
+      .then((json) => setElectronic(json))
+      .catch((error) => console.error(error));
+  };
+
+  const getMensClothingCategory = () => {
+    fetch(api_url + "/products/category/men's%20clothing")
+    .then((res) => res.json())
+      .then((json) => setMensClothing(json))
+      .catch((error) => console.error(error));
+  };
+
+  const getWomensClothingCategory = () => {
+    fetch(api_url + "/products/category/women's%20clothing")
+    .then((res) => res.json())
+      .then((json) => setWomensClothing(json))
+      .catch((error) => console.error(error));
+  };
+
   useEffect(() => {
     getProducts();
+    getJeweleryCategory();
+    getElectronicCategory();
+    getMensClothingCategory();
+    getWomensClothingCategory();
   }, []);
 
   return {
     products,
+    jewelery,
+    electronic,
+    mensClothing,
+    womensClothing,
   };
 };
 
