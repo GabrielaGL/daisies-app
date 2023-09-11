7eb0a0089df9feed88c7833cf9e9306c15c6045e
Gabriela Gomez <ggaabrielagomez@gmail.com>
2023-09-10 22:03:15 -0600

    Esqueleto de categorias

--- src/views/Categories.jsx
+++ src/views/Categories.jsx
@@ -1,12 +1,65 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Pressable, View, Image, StyleSheet } from "react-native";
+
+import CardOfProducts from "../components/cardOfProducts";
 
 const Categories = () => {
   return (
-    <View>
-      <Text> Categories funciona </Text>
+    <View style={styles.container}>
+      <View style={styles.rows}>
+        <Pressable style={styles.btnContainer}>
+          <Image
+            source={require("../assets/btnJewelery.png")}
+            style={styles.imgBtn}
+          />
+        </Pressable>
+
+        <Pressable style={styles.btnContainer}>
+          <Image
+            source={require("../assets/btnElectronics.png")}
+            style={styles.imgBtn}
+          />
+        </Pressable>
+      </View>
+
+      <View style={styles.rows}>
+        <Pressable style={styles.btnContainer}>
+          <Image
+            source={require("../assets/btnMensclothing.png")}
+            style={styles.imgBtn}
+          />
+        </Pressable>
+
+        <Pressable style={styles.btnContainer}>
+          <Image
+            source={require("../assets/btnWomensclothing.png")}
+            style={styles.imgBtn}
+          />
+        </Pressable>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignSelf: "center",
+    marginTop: 50,
+    backgroundColor: "#ADBDE6",
+  },
+  rows: {
+    flexDirection: "row",
+  },
+  btnContainer: {
+    width: 140,
+    height: 140,
+    margin: 15,
+  },
+  imgBtn: {
+    width: 140,
+    height: 140,
+  },
+});
 
 export default Categories;
