288babbb3952824a2f8a3d27c3846102f87f9be7
Gabriela Gomez <ggaabrielagomez@gmail.com>
2023-09-11 06:26:51 -0600

    Componente de carrito muestra contexto

--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import { useCart } from "../components/CartContext";
+
 const Cart = () => {
+  const { cart, deleteProductCart } = useCart();
+  console.log(cart);
+
   return (
     <View>
-      <Text> Cart funciona </Text>
+      <Text> Carrito se ve así </Text>
+      {cart.map((product) => (
+        <View key={product.title}>
+          <Text>Nombre: {product.title}</Text>
+          <Text>Precio: ${product.price}</Text>
+        </View>
+      ))}
     </View>
   );
 };
